cdc92f3c3f7e92068b013920d172aff5effe63c0
Darryn Codes <hello@darryncodes.co.uk>
2021-12-15 16:24:03 +0000

    functionality - updated logic to work for pricing and views

--- js/app.js
+++ js/app.js
@@ -2,71 +2,100 @@
 
 let views = [
   {
-    id: 0,
-    pageviews: '10K',
+    id: 8,
+    price: 8,
+    view: '10K',
   },
   {
-    id: 1,
-    pageviews: '50K',
+    id: 12,
+    price: 12,
+    view: '50K',
   },
   {
-    id: 2,
-    pageviews: '100K',
+    id: 16,
+    price: 16,
+    view: '100K',
   },
   {
-    id: 3,
-    pageviews: '500K',
+    id: 20,
+    price: 20,
+    view: '500K',
   },
   {
-    id: 4,
-    pageviews: '1M',
+    id: 24,
+    price: 24,
+    view: '1M',
   },
 ];
-// let price = [8, 12, 16, 20, 24];
-// let views = ['10k', '50k', '100k', '500k', '1m'];
 
 let slider = document.querySelector('input[type=range]');
 let btn = document.querySelector('input[type=checkbox]');
 const pageViews = document.querySelector('.views__page');
 const priceAmount = document.querySelector('.price__amount');
-// const pricePeriod = document.querySelector('.price__period');
 
-function figures() {
-  let value = parseInt(slider.value);
+function handleInput(e) {
+  let input = parseInt(e.target.value);
 
-  if (btn.checked) {
-    priceAmount.textContent = `$${value * 0.75}.00`;
-  } else if (btn.checked === false) {
-    priceAmount.textContent = `$${value * 1}.00`;
+  views.forEach(element => {
+    let price = element.price;
+    let totalViews = element.view;
+    let bg = input * 3;
+
+    if (price === input) {
+      priceAmount.textContent = `$${price}.00`;
+      pageViews.textContent = totalViews;
+      slider.style.background = `linear-gradient(90deg,
+      hsl(174, 76%, 80%) ${bg}%,
+     hsl(223, 50%, 87%) ${bg}%)`;
+    }
+  });
+
+  /*
+
+  if (input) {
+    priceAmount.textContent = `$${input}.00`;
   }
 
-  //   //   const view = views.values();
-  //   for (let obj of views) {
-  //     // pageViews.textContent = value;
-  //     console.log(value);
-  //     if (obj.id === obj.pageviews) {
-  //       pageViews.textContent = obj.pageviews;
-  //     }
-  //   }
+  if (input === '8') {
+    pageViews.textContent = '10k';
+    slider.style.background = `linear-gradient(90deg,
+      hsl(174, 76%, 80%) 0%,
+     hsl(223, 50%, 87%) 0%)`;
+  } else if (input === '12') {
+    pageViews.textContent = '50k';
+    slider.style.background = `linear-gradient(90deg,
+      hsl(174, 76%, 80%) 25%,
+     hsl(223, 50%, 87%) 25%)`;
+  } else if (input === '16') {
+    pageViews.textContent = '100k';
+    slider.style.background = `linear-gradient(90deg,
+      hsl(174, 76%, 80%) 50%,
+     hsl(223, 50%, 87%) 50%)`;
+  } else if (input === '20') {
+    pageViews.textContent = '500k';
+    slider.style.background = `linear-gradient(90deg,
+        hsl(174, 76%, 80%) 75%,
+       hsl(223, 50%, 87%) 75%)`;
+  } else if (input === '24') {
+    pageViews.textContent = '1M';
+    slider.style.background = `linear-gradient(90deg,
+        hsl(174, 76%, 80%) 100%,
+       hsl(223, 50%, 87%) 100%)`;
+  }
 
-  //   const iterator = views.values();
-
-  //   for (const value of iterator) {
-  //     console.log(value);
-  //   }
+  */
 }
 
-// function viewUpdate() {
-//   for (var i = 0; i < views.length; i++) {
-//     pageViews.textContent = views[i];
+// function handleClick(btn) {
+//   if (btn.checked === false) {
+//     priceAmount.textContent = (price / 100) * 1;
+//   } else if (btn.checked) {
+//     priceAmount.textContent = (price / 100) * 0.75;
 //   }
 // }
 
-btn.addEventListener('click', () => {
-  figures();
-});
-slider.addEventListener('input', () => {
-  figures();
-  //   viewUpdate();
-  //   pageViews.innerHTML = views[slider.value];
+btn.addEventListener('click', handleInput);
+
+slider.addEventListener('input', e => {
+  handleInput(e);
 });
