59d500277c7ba2ca5f0b63e7efd81a55134c8bb0
Darryn Codes <hello@darryncodes.co.uk>
2021-12-16 20:20:26 +0000

    Functionality - final js logic

--- js/app.js
+++ js/app.js
@@ -33,68 +33,36 @@ let btn = document.querySelector('input[type=checkbox]');
 const pageViews = document.querySelector('.views__page');
 const priceAmount = document.querySelector('.price__amount');
 
+let yearlyBilling = false;
+let price = views[2].price;
+
 function handleInput(e) {
   let input = parseInt(e.target.value);
 
-  views.forEach(element => {
-    let price = element.price;
-    let totalViews = element.view;
-    let bg = input * 3;
+  views.forEach((item, index) => {
+    price = item.price;
+    let totalViews = item.view;
+    let bg = index * 25;
 
     if (price === input) {
       priceAmount.textContent = `$${price}.00`;
       pageViews.textContent = totalViews;
       slider.style.background = `linear-gradient(90deg,
-      hsl(174, 76%, 80%) ${bg}%,
-     hsl(223, 50%, 87%) ${bg}%)`;
+        hsl(174, 76%, 80%) ${bg}%,
+       hsl(223, 50%, 87%) ${bg}%)`;
     }
   });
-
-  /*
-
-  if (input) {
-    priceAmount.textContent = `$${input}.00`;
-  }
-
-  if (input === '8') {
-    pageViews.textContent = '10k';
-    slider.style.background = `linear-gradient(90deg,
-      hsl(174, 76%, 80%) 0%,
-     hsl(223, 50%, 87%) 0%)`;
-  } else if (input === '12') {
-    pageViews.textContent = '50k';
-    slider.style.background = `linear-gradient(90deg,
-      hsl(174, 76%, 80%) 25%,
-     hsl(223, 50%, 87%) 25%)`;
-  } else if (input === '16') {
-    pageViews.textContent = '100k';
-    slider.style.background = `linear-gradient(90deg,
-      hsl(174, 76%, 80%) 50%,
-     hsl(223, 50%, 87%) 50%)`;
-  } else if (input === '20') {
-    pageViews.textContent = '500k';
-    slider.style.background = `linear-gradient(90deg,
-        hsl(174, 76%, 80%) 75%,
-       hsl(223, 50%, 87%) 75%)`;
-  } else if (input === '24') {
-    pageViews.textContent = '1M';
-    slider.style.background = `linear-gradient(90deg,
-        hsl(174, 76%, 80%) 100%,
-       hsl(223, 50%, 87%) 100%)`;
-  }
-
-  */
 }
 
-// function handleClick(btn) {
-//   if (btn.checked === false) {
-//     priceAmount.textContent = (price / 100) * 1;
-//   } else if (btn.checked) {
-//     priceAmount.textContent = (price / 100) * 0.75;
-//   }
-// }
+function handleClick(btn) {
+  yearlyBilling = btn.currentTarget.checked;
 
-btn.addEventListener('click', handleInput);
+  if (yearlyBilling) {
+    priceAmount.textContent = `$${price * 0.75}.00`;
+  } else priceAmount.textContent = `$${price * 1}.00`;
+}
+
+btn.addEventListener('click', handleClick);
 
 slider.addEventListener('input', e => {
   handleInput(e);
